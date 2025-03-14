e452cb370f48f4c7a282233c991db46420d31e3b
PriscaRevalina <friskarevalinamanurung@gmail.com>
2025-03-14 17:03:10 +0700

    PAIR 23

--- src/fintech/driver/Driver1.java
+++ src/fintech/driver/Driver1.java
@@ -1,15 +1,67 @@
 package fintech.driver;
-
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23025-Alif Aflah Suedi
+ * @author 12S23039-Prisca R. Manurung
  */
-public class Driver1 {
 
-    public static void main(String[] _args) {
+ import fintech.model.Account;
+ import fintech.model.Transaction;
+ import java.util.HashMap;
+ import java.util.Map;
+ import java.util.Scanner;
+ import java.lang.Double;
+ import java.lang.Math;
+ 
+   
+ public class Driver1 {
+    private static Map<String, Account> accounts = new HashMap<>();
 
-        // codes
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Read account creation input
+        String command = scanner.nextLine();
+        String owner = scanner.nextLine();
+        String accountName = scanner.nextLine();
+
+        // Create account and print details
+        createAccount(command, owner, accountName);
+
+        // Read transaction input
+        command = scanner.nextLine();
+        accountName = scanner.nextLine();
+        double amount = Double.parseDouble(scanner.nextLine());
+        String postedAt = scanner.nextLine();
+        String note = scanner.nextLine();
+        
+    
+
+        // Create transaction and print details
+        createTransaction(command, accountName, amount, postedAt, note);
+
+    
+    }
+    private static void createAccount(String command, String owner, String accountName) {
+        if ("create-account".equals("#")) {
+            Account account = new Account(command, owner, accountName);
+            accounts.put(accountName, account);
+            System.out.println(account.toString());
+         
+        }
 
     }
 
+    private static void createTransaction(String command, String accountName, double amount, String postedAt, String note) {
+        if ("create-transaction".equals( "#")) {
+            Account account = accounts.get(accountName);
+            if (account != null) {
+                Transaction transaction = new Transaction(accountName, amount, postedAt, note);
+                account.addTransaction(transaction);
+                System.out.println(transaction.toString() + "|" + account.getBalance());     
+
+            } 
+           
+
+        } 
+    }
 }
--- src/fintech/model/Account.java
+++ src/fintech/model/Account.java
@@ -1,11 +1,48 @@
 package fintech.model;
 
 /**
- * @author NIM Nama
- * @author NIM Nama
+ * @author 12S23025-Alif Aflah Suedi
+ * @author 12S23039-Prisca R. Manurung
  */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Account {
+    private final String owner;
+    private final String accountName;
+    private double balance;
+    private final List<Transaction> transactions;
 
-    // class definition
+    // Existing constructor
+    public Account(String owner, String accountName, double balance) {
+        this.owner = owner;
+        this.accountName = accountName;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
 
-}
+    // New constructor
+    public Account(String command, String owner, String accountName) {
+        this.owner = owner;
+        this.accountName = accountName;
+        this.balance = 0.0; // Default balance
+        this.transactions = new ArrayList<>();
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+        balance += transaction.getAmount();
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return accountName + "|" + owner + "|" + balance;
+    }
+
+} 
